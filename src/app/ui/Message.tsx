6c83fe6d62752591e3f3058f2e83d1ba565abe98
Ephraimiyanda <iyandaephraim@gmail.com>
2023-08-13 17:41:25 +0100

    configuring messaging between users using socket.io

--- src/app/ui/Message.tsx
+++ src/app/ui/Message.tsx
@@ -1,24 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import Image, { StaticImageData } from 'next/image';
+import Image from 'next/image';
 import Cookies from 'js-cookie';
 import io from 'socket.io-client';
-import useSWR, { mutate } from 'swr';
 
 // Interface declarations for TypeScript
 interface ContactIdProps {
   contactId: number;
 }
 
-interface MessageProps {
-  id: string | null;
-  sender: number;
-  receiver: number | string;
-  content: string;
-  timestamp: string;
-}
-
 interface UserProps {
-  avatar: string ;
+  avatar: string;
   name: string;
   id: number;
 }
@@ -26,23 +17,16 @@ interface UserProps {
 function Message({ contactId }: ContactIdProps) {
   // State to hold user information
   const [follower, setFollower] = useState<UserProps | null>(null);
-  const [inputValue,setInputValue]=useState("");
-  const [userMessage,setUserMessages]=useState<any>([]);
+  const [inputValue, setInputValue] = useState('');
+  const [userMessages, setUserMessages] = useState<string[]>([]);
   // Get user data from Cookies
   const userData = JSON.parse(Cookies.get('user') as string);
   const { avatar, name, id: userId } = follower || {};
-  const socket=io("https://ephraim-iyanda.onrender.com");
-
-
-  // Fetch user data using SWR
- 
-  useEffect(() => {
-    socket.on('messageResponse', (data) => setUserMessages([...userMessage, data]));
-  }, [socket, userMessage]);
+  const socket = io("https://ephraim-iyanda.onrender.com");
 
   // Fetch follower data when component mounts
   useEffect(() => {
-    fetchFollower();  
+    fetchFollower();
   }, []);
 
   // Fetch follower data
@@ -58,40 +42,34 @@ function Message({ contactId }: ContactIdProps) {
     }
   };
 
+  // Receive a message
+  useEffect(() => {
+    socket.on(`sender-${userData._id}`, (data: any) => {
+      setUserMessages(prevMessages => [...prevMessages, data.content]);
+    });
+  }, [socket, userData]);
+
   // Send a message
-  const sendMessage = async (messageContent: string) => {
-  const messageData = {
-        senderId: userData._id, // Make sure this matches your user schema
-        receiverId: '64cfcd0aa7d7451982ca8445', // Replace with actual receiver ID
-        content: messageContent,
-      };
+  const sendMessage = (messageContent: string) => {
+    const messageData = {
+      senderId: userData._id,
+      receiverId: '64cfcd0aa7d7451982ca8445', // Replace with actual receiver ID
+      content: messageContent,
+    };
+
     try {
-      setUserMessages([...userMessage, messageContent])
+      setUserMessages(prevMessages => [...prevMessages, messageContent]);
       socket.emit('sendMessage', messageData);
-     
     } catch (error) {
       console.log('An error occurred while sending the message:', error);
     }
   };
 
-  // Sort and display messages
-  const sortedMessages = userMessage?.messages
-    ?.filter(
-      (message: MessageProps) =>
-        (message.sender === Number(contactId) && message.receiver === Number(userId)) ||
-        (message.receiver === Number(contactId) && message.sender === Number(userId))
-    )
-    ?.sort((a: MessageProps, b: MessageProps) => {
-      const timeA = new Date(a.timestamp).getTime();
-      const timeB = new Date(b.timestamp).getTime();
-      return timeA - timeB;
-    });
-
   return (
     <div className="flex flex-col h-full">
       {/* Display user and avatar */}
       <div className="flex align-middle items-center gap-2 py-1 px-2 border-b border-b-stone-300">
-       {  avatar&&(
+        {avatar && (
           <Image
             className="rounded-[50%] w-[54px] h-[54px]"
             src={avatar}
@@ -99,35 +77,54 @@ function Message({ contactId }: ContactIdProps) {
             width={100}
             height={100}
             priority
-          />)}
-      
+          />
+        )}
+
         <p className="text-lg">{name}</p>
       </div>
       {/* Display messages */}
       <div className="h-[75.3vh] sm:h-[78vh] overflow-y-auto block">
-        {userMessage}
+        {userMessages.map((message, index) => (
+          <div
+            key={index}
+            className={`p-1 pl-2 pr-2 rounded-lg mt-2 w-fit ${
+              userId === contactId ? 'bg-green-300 ml-auto' : 'bg-red-300'
+            }`}
+          >
+            <p>{message}</p>
+          </div>
+        ))}
       </div>
       {/* Message input */}
-      <form className="message-input w-full border-t items-center border-stone-300 fixed bottom-[0] flex" 
-      onSubmit={(e)=>{
-        e.preventDefault()
-        sendMessage(inputValue);
-        setInputValue("")
-      }}>
+      <form
+        className="message-input w-full border-t items-center border-stone-300 fixed bottom-[0] flex"
+        onSubmit={(e) => {
+          e.preventDefault();
+          sendMessage(inputValue);
+          setInputValue('');
+        }}
+      >
         <input
           type="text"
           className="w-full px-4 py-2"
+          placeholder="Type a message..."
           value={inputValue}
-          onChange={(e)=>{setInputValue(e.target.value)}}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
           onKeyDown={(e) => {
             if (e.key === 'Enter') {
               sendMessage(inputValue);
-              e.currentTarget.value = '';
-              setInputValue("");
+              setInputValue('');
             }
           }}
         />
-        <button type='submit' className=' bg-black text-white w-10 p-1 rounded h-10 right-[20px] sticky'>→</button>
+        <button
+          type="submit"
+          className="bg-black text-white w-10 p-1 rounded h-10 right-[20px] sticky"
+        >
+          →
+        </button>
       </form>
     </div>
   );

83ae140b0e872cfa43584a3dc9f0f152911b5cfc
Ephraimiyanda <iyandaephraim@gmail.com>
2023-08-16 16:30:38 +0100

    configuring messaging between users using socket.io

--- src/app/ui/Message.tsx
+++ src/app/ui/Message.tsx
@@ -47,7 +47,7 @@ function Message({ contactId }: ContactIdProps) {
   const sendMessage = (messageContent: string) => {
     const messageData = {
       senderId: userData._id,
-      receiverId: "64d90b7cf1cefce483e79244", // Replace with actual receiver ID
+      receiverId: "64c822dd49065021d3a30e4f", // Replace with actual receiver ID
       content: messageContent,
     };
 

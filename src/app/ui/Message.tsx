ecaf95823f3c81bee46ce672cc12149c4b59b1ca
Ephraimiyanda <iyandaephraim@gmail.com>
2023-08-17 10:12:44 +0100

    configuring messaging between users using socket.io

--- src/app/ui/Message.tsx
+++ src/app/ui/Message.tsx
@@ -39,7 +39,7 @@ function Message({ contactId }: ContactIdProps) {
 
   useEffect(() => {
 
-    socket.on(`sender-${userData._id}`, (data: any) => {
+    socket.on(`receive-${userData._id}`, (data: any) => {
       setUserMessages((prevMessages) => [...prevMessages, { content: data.content, fromSelf: false }]);
     });
 
